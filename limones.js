8e30c0ccb85db7a24fe8d1c1f265b179e93b2960
fabricioquillupangui1 <fquillupangui19@gmail.com>
2026-09-07 12:27:08 -0500

    posicion aleatorio del limon

--- limones.js
+++ limones.js
@@ -12,8 +12,7 @@ let limonY=0;
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
-    dibujarLimon();
-    bajarLimon();
+    aparecerLimon();
 }
 function dibujarSuelo(){
     ctx.fillStyle="blue";
@@ -56,12 +55,27 @@ function dibujarLimon(){
 function bajarLimon(){
 limonY = limonY +10;
 actualizarPantalla();
+detectarColision();
 }
 
 function detectarColision (){
-    if(limonX + ANCHO_LIMON>personajeX && limonX<personajeX + ANCHO_PERSONAJE &&
-        limonY + ALTURA_LIMON>personajeY && limonY<personajeY + ALTURA_PERSONAJE
+    if(limonX + ANCHO_LIMON>personajeX && 
+        limonX<personajeX + ANCHO_PERSONAJE &&
+        limonY + ALTURA_LIMON>personajeY && 
+        limonY<personajeY + ALTURA_PERSONAJE
     ){
-alert("Atrapado!!");
+        //alert("ATRAPADO!!");
+aparecerLimon();
     }
+}
+
+function probarAleatorio(){
+    let aleatorio=generarAleatorio(10,80);
+    console.log(aleatorio);
+}
+
+function aparecerLimon(){
+limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
+limonY=0;
+actualizarPantalla();
 }
