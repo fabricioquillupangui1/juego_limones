d0e330c0d878b148ac4aa88d59663c086913b8f3
fabricioquillupangui1 <fquillupangui19@gmail.com>
2026-09-07 14:20:49 -0500

    agregar niveles de velocidad y mensaje de ganador en detectarAtrapado

--- limones.js
+++ limones.js
@@ -74,6 +74,9 @@ aparecerLimon();
 puntaje = puntaje+1;
 mostrarEnSpan("txtPuntaje",puntaje);
     }
+    if(puntaje==3){velocidadCaida=150;}
+    if(puntaje==6){velocidadCaida=100;}
+    if(puntaje == 10){ alert("¡TIENES LOS LIMONES, AHORA TE FALTA SAL Y TEQUILA!"); }
 }
 
 function detectarPiso(){
